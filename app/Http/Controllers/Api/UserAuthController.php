fd47f824670acd638add384c39bfebac492ba09d
meekaii <cb010737@students.apiit.lk>
2024-05-13 03:52:28 +0530

    initial commit

---
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    //OrGx4SLaviWII95GjT2ZsTxVEKjFVKagkTDXAzrBfd7cdb8a
+    public function getUser(){
+        return 'USER !!';
+    }
+}
